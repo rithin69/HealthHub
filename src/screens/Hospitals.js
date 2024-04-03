09a2b890218b65e836c6fee14e01a63e1a72e93b
rithin69 <69008632+rithin69@users.noreply.github.com>
2024-04-03 17:19:45 +0100

    updated package.json and hospital component

--- src/screens/Hospitals.js
+++ src/screens/Hospitals.js
@@ -85,7 +85,7 @@ const Hospitals = () => {
                 ))}
             </div>
             {isModalOpen && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">a
                     <div className="bg-white p-5 rounded-lg max-w-md mx-auto">
                         <p>Congratulations!! You have selected {hospitals.find(hospital => hospital.id === selectedHospitalId)?.name}. Waiting for admin's approval.</p>
                         <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 float-right" onClick={closeModal}>Close</button>
